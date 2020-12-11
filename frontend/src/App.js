aab812291b65498550225efe5a6eab88332a7809
FranciscaFigueiredo <48726063+FranciscaFigueiredo@users.noreply.github.com>
2020-12-11 20:14:03 -0300

    axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,10 @@
 import {useState} from "react"
+import User from './pages/User'
 
 function App() {
-  const [counter, setCounter] = useState(0)
-
-  function increment() {
-    setCounter(counter + 1)
-  }
   return (
     <div className="App">
-      <div>
-        contador: {counter}
-      </div>
-      <button onClick = {increment}>Incremento</button>
+      <User/>
     </div>
   );
 }
