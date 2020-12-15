3b69fc2a8d639455d2855432eaebb3f89840e528
FranciscaFigueiredo <48726063+FranciscaFigueiredo@users.noreply.github.com>
2020-12-15 00:37:19 -0300

    atualização do frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Header from './pages/Header'
-import reportWebVitals from './reportWebVitals';
-import server from './..server'
-const cors = require('cors')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(
+ReactDOM.render (
   <React.StrictMode>
-    <Header title = 'Ecoleta'/>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-app.use(cors())
-app.use(server)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById('root'),
+)
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import User from './pages/Search-results'
-import Profile from './pages/Profile'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-export default function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact = "/" component = {User}/>
-                <Route path = "/create" component = {Profile}/>
-                <Route path = "/update/:id" component = {Profile}/>
+import Home from '../pages/Home';
+import CreatePoint from '../pages/CreatePoint';
 
-            </Switch>
-        </BrowserRouter>
-    )
-}
+const Routes = () => {
+  return (
+    <Switch>
+      <Route component={Home} path="/" exact />
+      <Route component={CreatePoint} path="/create-point" />
+    </Switch>
+  );
+};
+
+export default Routes;
